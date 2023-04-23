71c2a178c653d5795dbc4a9dcaff131561ff9dbd
lizzyraesetja <lizzraesetja2@gmail.com>
2023-04-23 07:18:52 -0700

    0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -39,5 +39,6 @@ void bubble_sort(int *array, size_t size)
 				bubbly = false;
 			}
 		len--;
+		}
 	}
 }
--- print_array.c
+++ print_array.c
@@ -21,3 +21,4 @@ void print_array(const int *array, size_t size)
 		++i;
 	}
 	printf("\n");
+}
